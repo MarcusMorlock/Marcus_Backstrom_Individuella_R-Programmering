6b70b8fedb38c89139988ec1d757741066255d78
Marcus Backstrom <VBMarcusVB@hotmail.com>
2026-04-06 16:49:39 +0200

    Cleaning

--- Marcus_Ar_Bast_Script1.R
+++ Marcus_Ar_Bast_Script1.R
@@ -1,1 +1,34 @@
-library(tidyverse)
+load_data <- function(csv_path = "insurance_costs.csv"){
+  library(tidyverse)
+  df <- read_csv(csv_path)
+  glimpse(df)
+}
+
+clean_data <- function(df) {
+  df_clean <- df %>% 
+    mutate(
+      customer_id = str_replace(customer_id, "C", ""),
+      customer_id = as.integer(customer_id),
+      age = as.integer(age),
+      children = as.integer(children),
+      sex = str_to_lower(sex),
+      region = str_to_lower(region),
+      smoker = str_to_lower(smoker),
+      chronic_condition = str_to_lower(chronic_condition),
+      exercise_level = str_to_lower(exercise_level),
+      plan_type = str_to_lower(plan_type)
+    )
+ glimpse(df_clean)
+}
+
+print_unique_chars_only <- function(df, skip_col = "customer_id") {
+  
+  for (col in setdiff(names(df), skip_col) ) {
+    
+    if (is.character(df[[col]])) {
+      print(col)
+      print(unique(df[[col]]))
+    }
+  }
+  colSums(is.na(df))
+}
---
+++ Marcus_Ar_Bast_Testing_Script.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+df <- read_csv("insurance_costs.csv")
+
+glimpse(df)
+df_clean <- df %>% 
+  mutate(
+    customer_id = str_replace(customer_id, "C", ""),
+    customer_id = as.integer(customer_id),
+    age = as.integer(age),
+    children = as.integer(children)
+  )
+
+glimpse(df_clean)
+
+for (col in names(df_clean)) {
+  if (is.character(df_clean[[col]])) {
+    print(unique(df_clean[[col]]))
+  }
+}
