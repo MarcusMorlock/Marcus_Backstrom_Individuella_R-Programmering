e041f223a17e1fb3e0e67d1987b7cbf0c817b19c
Marcus Backstrom <VBMarcusVB@hotmail.com>
2026-04-10 16:03:51 +0200

    Added age_group, bmi_category, health_risk_level.

--- scripts/Marcus_Ar_Bast_Testing_Script.R
+++ scripts/Marcus_Ar_Bast_Testing_Script.R
@@ -1,19 +1,50 @@
-library(tidyverse)
-df <- read_csv("data/insurance_costs.csv")
+source("scripts/load_data.R")
+df_raw <- load_data("data/insurance_costs.csv")
 
-glimpse(df)
-df_clean <- df %>% 
+source("scripts/utility.R")
+
+source("scripts/clean_data.R")
+df_clean <- clean_data(df_raw)
+
+look_at_data(df = df_clean)
+
+
+
+
+
+df_add_column <- df_clean %>% 
   mutate(
-    customer_id = str_replace(customer_id, "C", ""),
-    customer_id = as.integer(customer_id),
-    age = as.integer(age),
-    children = as.integer(children)
+    bmi_category = case_when(
+      bmi < 18.5 ~ "under weight",
+      bmi < 25 ~ "normal weight",
+      bmi < 30 ~ "over weight",
+      bmi < 35 ~ "obese",
+      TRUE ~ "severely obese"
+    ),
+    bmi_category = factor(bmi_category, levels = c("under weight","normal weight", "over weight", "obese", "severely obese" ))
+  )
+look_at_data(df = df_add_column)
+
+df_add_column <- df_add_column %>%
+  mutate(
+    health_risk_level = case_when(
+      
+      #High Risk
+      smoker == TRUE | 
+      chronic_condition == TRUE |
+      bmi_category == "severely obese"|
+      (bmi_category == "obese" & exercise_level == "low")  ~ "high risk",
+      
+      #Medium Risk
+      bmi_category == "obese" |
+      (bmi_category == "over weight" & exercise_level == "low") |
+      (age_group == "45-59" & exercise_level == "low") ~ "medium risk",
+      
+      #Low Risk
+      TRUE ~ "low risk"
+    ),
+    health_risk_level = factor(health_risk_level, levels = c("low risk","medium risk", "high risk" ))
   )
 
-glimpse(df_clean)
-
-for (col in names(df_clean)) {
-  if (is.character(df_clean[[col]])) {
-    print(unique(df_clean[[col]]))
-  }
-}
+summary(df_add_column)
+look_at_data(df_add_column)
---
+++ scripts/add_column.R
@@ -0,0 +1,47 @@
+bmi_category <- function(df) {
+  df_add_bmi_category<- df %>% 
+    mutate(
+      bmi_category = case_when(
+        bmi < 18.5 ~ "under weight",
+        bmi < 25 ~ "normal weight",
+        bmi < 30 ~ "over weight",
+        bmi < 35 ~ "obese",
+        TRUE ~ "severely obese"
+      ),
+      bmi_category = factor(bmi_category, levels = c("under weight","normal weight", "over weight", "obese", "severely obese" ))
+    )
+  return(df_add_bmi_category)
+}
+
+health_risk_level <- function(df) {
+  df_add_health_risk_level <- df %>%
+    mutate(
+      health_risk_level = case_when(
+        
+        #High Risk
+        smoker == TRUE | 
+          chronic_condition == TRUE |
+          bmi_category == "severely obese"|
+          (bmi_category == "obese" & exercise_level == "low")  ~ "high risk",
+        
+        #Medium Risk
+        bmi_category == "obese" |
+          (bmi_category == "over weight" & exercise_level == "low") |
+          (age_group == "45-59" & exercise_level == "low") ~ "medium risk",
+        
+        #Low Risk
+        TRUE ~ "low risk"
+      ),
+      health_risk_level = factor(health_risk_level, levels = c("low risk","medium risk", "high risk" ))
+    )
+  return(df_add_health_risk_level)
+}
+
+# add_age_group <- function(age) {
+#  case_when(
+#       age < 30 ~ "under 30",
+#       age < 45 ~ "30-44",
+#       age < 60 ~ "45-59",
+#       TRUE ~ "60 and above"
+#     )
+# }
