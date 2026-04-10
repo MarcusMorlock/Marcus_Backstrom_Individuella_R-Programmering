efbc3a4f210e306507bd06e5cbc59206c62685e5
Marcus Backstrom <VBMarcusVB@hotmail.com>
2026-04-10 16:19:49 +0200

    comment on cleanup done.

--- scripts/Marcus_Ar_Bast_Testing_Script.R
+++ scripts/Marcus_Ar_Bast_Testing_Script.R
@@ -8,43 +8,5 @@ df_clean <- clean_data(df_raw)
 
 look_at_data(df = df_clean)
 
+source("scripts/add_column.R")
 
-
-
-
-df_add_column <- df_clean %>% 
-  mutate(
-    bmi_category = case_when(
-      bmi < 18.5 ~ "under weight",
-      bmi < 25 ~ "normal weight",
-      bmi < 30 ~ "over weight",
-      bmi < 35 ~ "obese",
-      TRUE ~ "severely obese"
-    ),
-    bmi_category = factor(bmi_category, levels = c("under weight","normal weight", "over weight", "obese", "severely obese" ))
-  )
-look_at_data(df = df_add_column)
-
-df_add_column <- df_add_column %>%
-  mutate(
-    health_risk_level = case_when(
-      
-      #High Risk
-      smoker == TRUE | 
-      chronic_condition == TRUE |
-      bmi_category == "severely obese"|
-      (bmi_category == "obese" & exercise_level == "low")  ~ "high risk",
-      
-      #Medium Risk
-      bmi_category == "obese" |
-      (bmi_category == "over weight" & exercise_level == "low") |
-      (age_group == "45-59" & exercise_level == "low") ~ "medium risk",
-      
-      #Low Risk
-      TRUE ~ "low risk"
-    ),
-    health_risk_level = factor(health_risk_level, levels = c("low risk","medium risk", "high risk" ))
-  )
-
-summary(df_add_column)
-look_at_data(df_add_column)
